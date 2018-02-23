bc8f3c20b24bd49f1c88db0f58a9298fbe430686
Perry Spratt <perrywespratt@gmail.com>
2018-02-23 14:32:39 -0800

    Added batching ability to fix_zstacks

--- BenderBox/Z-stacks/fix_zstacks.m
+++ BenderBox/Z-stacks/fix_zstacks.m
@@ -3,9 +3,21 @@ function fix_zstacks
 %Function to fix the issue in prairie that randomly doubles the intensity
 %of frames in a zstack
 
+zstack_folders = uipickfiles();
 
-%select Directory
-folder_name = uigetdir;
+for i=1:length(zstack_folders)
+    try
+        fix_zstack(zstack_folders{i});
+    catch
+        warning(['Could not fix ' zstack_folders{i}]);
+    end
+    
+end
+
+end
+
+function fix_zstack(folder_name)
+
 %load tiff files
 image_files = dir([folder_name filesep '*.tif']);
 
@@ -17,7 +29,7 @@ for i=1:length(image_files)
 
 end
 
-mkdir([folder_name filesep 'Fixed Zseries']);
+mkdir([folder_name filesep 'Original Image Files']);
 figure;
 hold on;
 plot(mean_image_data);
@@ -26,6 +38,8 @@ threshold = nanmedian(mean_image_data)*1.5;
 
 for i=1:length(image_files)
     image_data = imread([folder_name filesep image_files(i).name]);
+    imwrite(image_data,[folder_name filesep 'Original Image Files' filesep image_files(i).name])
+    
     sorted_pixels = sort(image_data(:));
     
     if mean(sorted_pixels(1:length(sorted_pixels)/4)) >= threshold
@@ -34,8 +48,9 @@ for i=1:length(image_files)
    
     sorted_pixels = sort(image_data(:));
     mean_image_data(i) = mean(sorted_pixels(1:length(sorted_pixels)/4));
-    imwrite(image_data,[folder_name filesep 'Fixed Zseries' filesep image_files(i).name])
+    imwrite(image_data,[folder_name filesep image_files(i).name])
 end
 
 plot(mean_image_data);
+end
 
